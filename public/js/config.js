e0311b53469fef2700849a99c9bdd92acaf3fa7e
Forgottenrealms <1456671936@qq.com>
2018-12-21 14:05:40 +0800

    feat:首页更新

--- public/js/config.js
+++ public/js/config.js
@@ -3,7 +3,8 @@ require.config({
     paths: {
         jquery: "/lib/jquery/jquery-1.12.4.min",
         bootstrap: "/lib/bootstrap/js/bootstrap.min",
-        load: "/js/loadHeader"  //加载头部
+        load: "/js/loadHeader",  //加载头部
+        swiper: "/lib/swiper/js/swiper.min"
     },
     shim: {
         bootstrap: {
--- public/js/home/index.js
+++ public/js/home/index.js
@@ -1,5 +1,38 @@
 require(["../config"], ()=>{
-    require(["jquery", "load", "bootstrap"], function($) {
+    require(["jquery", "swiper", "load", "bootstrap"], function($, Swiper) {
+        function Index() {
+			this.loadSwiper();
+		}
 
+		$.extend(Index.prototype, {
+			// Swiper 轮播
+			loadSwiper() {
+				var mySwiper = new Swiper ('.swiper-container', {
+				    // direction: 'vertical', // 垂直切换选项
+				    loop: true, // 循环模式选项
+				    autoplay: { // 自动轮播
+				    	delay: 1000
+				    },
+				    
+				    // 如果需要分页器
+				    pagination: {
+				      el: '.swiper-pagination',
+				    },
+				    
+				    // 如果需要前进后退按钮
+				    // navigation: {
+				    //   nextEl: '.swiper-button-next',
+				    //   prevEl: '.swiper-button-prev',
+				    // },
+				    
+				    // 如果需要滚动条
+				    // scrollbar: {
+				    //   el: '.swiper-scrollbar',
+				    // },
+				  })        
+			}
+		});
+
+		new Index();
     });
 });
